72af1f4f9ff1feb006e239738558607545ef1d13
Brett Hawkins <bhawk123@gmail.com>
2018-02-20 20:49:05 +0000

    Added Unit Testing

--- cmd.sh
+++ cmd.sh
@@ -5,6 +5,9 @@ version="identidock-v1.py"
 if [ "$ENV" = 'DEV' ]; then
     echo "Running Development Server"
     exec python ${version}
+elif [ "$ENV" = 'UNIT' ]; then
+    echo "Running Unit Tests"
+    exec python "tests.py"
 else 
     echo "Running Production Server"
     exec uwsgi -http 0.0.0.0:9090 --wsgi-file /app/${version} --callable app --stats 0.0.0.0:9191
