e135ec30a719b908b29f500af815f14caf7fe1cf
Brett Hawkins <bhawk123@gmail.com>
2018-02-22 19:44:28 -0800

    jenkins.yml fix

--- dock-build-tag-push.sh
+++ dock-build-tag-push.sh
@@ -15,8 +15,10 @@ docker build --no-cache -t ${account}/${repository}:${1} .
 
 echo "Tagging image"
 docker tag "identidock" "${account}/${repository}:${1}"
+docker tag "identidock" "${account}/${repository}:newest"
 
 echo "NOT Pushing image to Docker"
-# docker push "${account}/${repository}:${1}"
+docker push "${account}/${repository}:${1}"
+docker push "${account}/${repository}:newest"
 
 echo "DONE"
